e61cb01bd77f20301d8aa419b7722dfc88fc7984
Rusty Eddy <rusty@rustyeddy.com>
2018-09-30 16:10:13 -0700

    Added start and end to the walk routine

--- app/walk.go
+++ app/walk.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/gocolly/colly"
 	"github.com/gorilla/mux"
@@ -12,6 +13,8 @@ import (
 type pageInfo struct {
 	StatusCode int
 	Links      map[string]int
+	Start      time.Time
+	End        time.Time
 }
 
 func handleWalk(w http.ResponseWriter, r *http.Request) {
@@ -20,18 +23,13 @@ func handleWalk(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	url := vars["url"]
 
-	// This is standard {url}
-	// url := r.URL.Query().Get("url")
-	// if url == "" {
-	// 	log.Errorln("missing URL")
-	// 	return
-	// }
-
+	// TODO: check for "http://" prefix
 	url = "http://" + url
 	log.Infoln("visiting", url)
 
+	// Create the collector and go get shit!
 	c := colly.NewCollector(
-		colly.MaxDepth(3),
+		colly.MaxDepth(1),
 	)
 	p := &pageInfo{Links: make(map[string]int)}
 
@@ -44,15 +42,19 @@ func handleWalk(w http.ResponseWriter, r *http.Request) {
 	})
 
 	c.OnResponse(func(r *colly.Response) {
-		log.Infoln("response recieved", r.Request.URL, r.StatusCode)
+		link := r.Request.URL
+		log.Infoln("response recieved", link, r.StatusCode)
 		p.StatusCode = r.StatusCode
+		p.End = time.Now()
 	})
 
 	c.OnError(func(r *colly.Response, err error) {
 		log.Infoln("error:", r.StatusCode, err)
 		p.StatusCode = r.StatusCode
+		p.End = time.Now()
 	})
 
+	p.Start = time.Now()
 	c.Visit(url)
 
 	b, err := json.Marshal(p)
