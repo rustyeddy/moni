4d4f1e7c6edcd1a6b310b0460ddb212e66bad75d
Rusty Eddy <rusty@rustyeddy.com>
2018-10-15 19:13:32 -0700

    Set content type to html as expected
    This is a longer description, I take it?

--- app.go
+++ app.go
@@ -7,5 +7,6 @@ import (
 
 func AppHandler(w http.ResponseWriter, r *http.Request) {
 	// Serve up the website slash app
-	fmt.Fprintln(w, "Hello, World")
+	w.Header().Add("Content-Type", "text/html")
+	fmt.Fprintln(w, "<h1>Hello, World</h1>")
 }
