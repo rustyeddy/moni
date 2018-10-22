e7aff39ef97cf6f6dd11100d2eb735fcd558258c
Rusty Eddy <rusty@rustyeddy.com>
2018-10-22 03:25:10 -0700

    we are going to keep main and mix in the templates

---
+++ site_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestSiteListHandler(t *testing.T) {
+
+	url := "/sites"
+	resp := ServiceTester(t, SiteListHandler, "get", url)
+
+	body := GetBody(resp)
+	if body == nil {
+		t.Errorf("crawl list read body failed")
+	}
+	ctype := resp.Header.Get("Content-Type")
+	if ctype != "application/json" {
+		t.Errorf("expected content type (application/json) got (%s)", ctype)
+	}
+
+	var sites Sitemap
+	err := json.Unmarshal(body, &sites)
+	if err != nil {
+		t.Errorf("failed unmarshallng sites %v ", err)
+	}
+
+	fmt.Printf("sites %+v\n", sites)
+	if len(sites) < 1 {
+		t.Errorf("failed should have more sites")
+	}
+}
+
+func TestSiteIdHandler(t *testing.T) {
+	url := "/site/"
+	var body []byte
+
+	resp := ServiceTester(t, SiteIdHandler, "get", url+"/"+url)
+	if body = GetBody(resp); body == nil {
+		t.Errorf("failed to get body from response")
+	}
+	fmt.Printf("site %s %+v", resp.Request.URL, body)
+}
