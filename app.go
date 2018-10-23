7fc339449d6b9427b8cd049d98d9d5d11793ebbf
Rusty Eddy <rusty@rustyeddy.com>
2018-10-22 19:42:59 -0700

    moded some stuff

--- app.go
+++ app.go
@@ -4,7 +4,20 @@ import (
 	"net/http"
 
 	"github.com/rustyeddy/moni/app"
+	log "github.com/sirupsen/logrus"
 )
+
+type AppData struct {
+	Name    string
+	Frag    string
+	Content string
+	Title   string
+
+	Crawls  []*moni.Crawls
+	Sites   *moni.Sitemap
+	Pages   *moni.Pages
+	Domains *moni.Domains
+}
 
 // AppHandler will compose a response to the request
 // and in the process most likely need to gather a few peices
@@ -12,22 +25,19 @@ import (
 // send back to the caller
 func AppHandler(w http.ResponseWriter, r *http.Request) {
 
+	log.Infoln("AppHandler request: %+v", r)
+
 	b := app.NewBuilder()
+	d := AppData{
+		Name:    "Rusty Eddy",
+		Frag:    r.URL.Fragment,
+		Content: "Hello, World!",
+		Title:   "Clowd Ops",
 
-	data := struct {
-		Title string
-		Name  string
-
-		// Maybe?
-		Crawls []string
-		Sites  Sitemap
-		Pages  Pagemap
-	}{
-		Title:  "ClowdOps",
-		Crawls: GetCrawls(),
-		Sites:  GetSites(),
-		Pages:  GetPages(),
+		Crawls:  moni.GetCrawls(),
+		Sites:   monilGetSites(),
+		Pages:   moni.GetPages(),
+		Domains: moni.GetDomains(),
 	}
-
 	b.Assemble(w, "index.html", data)
 }
