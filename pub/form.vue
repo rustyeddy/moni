ca8c82dd385f6e63f7ab75fe6de80d7307c66a8e
Rusty Eddy <rusty@rustyeddy.com>
2019-12-17 13:31:51 -0800

    added the form vue

---
+++ pub/form.vue
@@ -0,0 +1,15 @@
+<template>
+    <!-- make this a modal -->
+    <form id="walk" method="post" class="m-4" action="/">
+	<div class="form-group row">
+	    <div class="col-sm-10">
+		<input type="text" class="form-control m-2" placeholder="example.com">
+		<button type="submit" value="submit" class="btn btn-primary btn-lg btn-block m-2">Walk That URL</button>
+ 	    </div>
+	</div>
+    </form>
+</template>
+
+<script>
+ 
+</script>
